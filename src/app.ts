591771d0cb5a644c11ffa8eb5ac424a2dc064f17
Dariusz Filipiak <darekf77@gmail.com>
2026-01-03 23:47:51 +0100

    $$$ update $$$

--- src/app.ts
+++ src/app.ts
@@ -174,10 +174,10 @@ export const TaonElectronAppAppConfig: ApplicationConfig = {
     provideBrowserGlobalErrorListeners(),
     provideRouter(TaonElectronAppClientRoutes),
     provideClientHydration(withEventReplay()),
-    provideServiceWorker('ngsw-worker.js', {
-      enabled: !isDevMode(),
-      registrationStrategy: 'registerWhenStable:30000',
-    }),
+    // provideServiceWorker('ngsw-worker.js', {
+    //   enabled: !isDevMode(),
+    //   registrationStrategy: 'registerWhenStable:30000',
+    // }),
   ],
 };
 
